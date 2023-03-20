13f63482bab9c65d6dc398cfefab2595a8207a3c
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-03-19 23:24:54 -0500

    try to push

---
+++ test.js
@@ -0,0 +1,11 @@
+// Change code below this line
+function add(...args) {
+  let total = 0;
+  for (let arg of args) {
+    total += arg;
+  }
+  console.log(total);
+  // Change code above this line
+}
+
+add(15, 27, 10, 8, 14);
